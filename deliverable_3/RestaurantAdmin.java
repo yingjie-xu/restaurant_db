383426092a8654f9219eafdd6be3153d7b5944f8
Yingjie Xu <xuyingjie619@gmail.com>
2020-03-15 20:09:20 -0400

    Add more staff operations

--- deliverable_3/RestaurantAdmin.java
+++ deliverable_3/RestaurantAdmin.java
@@ -1,12 +1,13 @@
-import java.sql.*;
+/**
+ * Group 62 project
+ * members: Weiming Guo, Hengxian Jiang, Yingjie Xu, Helen Ren
+ */
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.Scanner;
 
 /**
@@ -19,7 +20,6 @@ import java.util.Scanner;
  *
  **/
 
-
 public class RestaurantAdmin {
 
     private static String url = "jdbc:postgresql://comp421.cs.mcgill.ca:5432/cs421";
@@ -29,6 +29,11 @@ public class RestaurantAdmin {
 
     private static Scanner sc = new Scanner(System.in);
 
+    /**
+     * Start the restaurant management system program
+     * @param args
+     * @author Yingjie Xu
+     */
     public static void main(String[] args) {
         try {
             DriverManager.registerDriver ( new org.postgresql.Driver() ) ;
@@ -46,6 +51,7 @@ public class RestaurantAdmin {
             System.out.println("0 - Quit the application");
             System.out.println("Just type the number of the option you want:");
             int select = sc.nextInt();
+            sc.nextLine();
             if (select == 0) {
                 break;
             } else if (select == 1) {
@@ -61,15 +67,19 @@ public class RestaurantAdmin {
             }
             System.out.println("---------------------------------");
             System.out.println("Do you want to continue? (y/n): ");
-            String input = sc.nextLine(); // this line is not used
-            input = sc.nextLine();
+            String input = sc.nextLine();
             if (!input.toLowerCase().equals("y")) {
                 break;
             }
         }
         System.out.println("Thanks for using the restaurant management system!");
+        sc.close();
     }
 
+    /**
+     * All possible methods for the admin to modify the staff
+     * @author Yingjie Xu
+     */
     public static void staff() {
         System.out.println("---------- Staff ----------");
         System.out.println("Please select from following options: ");
@@ -81,12 +91,34 @@ public class RestaurantAdmin {
         System.out.println("6 - Modify the proficiency of a chef");
         System.out.println("7 - View all the staff");
         int select = sc.nextInt();
+        sc.nextLine();
         if (select == 1) {
             addChef();
+        } else if (select == 2) {
+            addDeliveryGuy();
+        } else if (select == 3) {
+            addWaiter();
+        } else if (select == 4) {
+            modifySalary();
+        } else if (select == 5) {
+
+        } else if (select == 6) {
+
+        } else if (select == 7) {
+
+        } else {
+            System.out.println("Invalid selection.");
+            return;
         }
     }
 
-    public static void addChef() {
+    /**
+     * Helper method for addChef, addDeliveryGuy and addWaiter
+     * Add a staff into database
+     * @return sid
+     * @author Yingjie Xu
+     */
+    public static int addStaff() {
         Connection con = null;
         Statement statement = null;
         ResultSet rs = null;
@@ -95,12 +127,145 @@ public class RestaurantAdmin {
             statement = con.createStatement();
             rs = statement.executeQuery("SELECT sid FROM staff ORDER BY sid DESC LIMIT 1");
             rs.next();
-            System.out.println("Max sid = " + rs.getString(1));
+            int sid = rs.getInt(1);
+            sid++;
+            System.out.println("----- Info of Staff -----");
+            System.out.println("Name of the staff: ");
+            String name = sc.nextLine();
+            System.out.println("Working schedule of the staff (choose from morning, afternoon, evening): ");
+            String schedule = sc.nextLine();
+            System.out.println("Salary of the staff: ");
+            double salary = sc.nextDouble();
+            sc.nextLine();
+            int result = statement.executeUpdate("INSERT INTO staff VALUES(" + sid + ",'" + name + "','" + schedule + "'," + salary + ");");
+            if (result == 1) {
+                System.out.println("New staff added successfully.");
+            } else {
+                System.out.println("Something went wrong. New staff was not added.");
+            }
+            return sid;
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try { rs.close(); } catch (Exception e) { /* ignored */ }
+            try { statement.close(); } catch (Exception e) { /* ignored */ }
+            try { con.close(); } catch (Exception e) { /* ignored */ }
+        }
+        return -1;
+    }
+
+    /**
+     * Add a chef into database
+     * @author Yingjie Xu
+     */
+    public static void addChef() {
+        Connection con = null;
+        Statement statement = null;
+        try {
+            con = DriverManager.getConnection(url, usernamestring, passwordstring);
+            statement = con.createStatement();
+            int sid = addStaff();
+            if (sid == -1) {
+                System.out.println("Fail to add the staff.");
+                return;
+            }
+            System.out.println("----- Info of Chef -----");
+            System.out.println("Proficiency of the chef (1 - 5): ");
+            int proficiency = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Cooking style of the chef: ");
+            String style = sc.nextLine();
+            int result = statement.executeUpdate("INSERT INTO chef VALUES(" + proficiency + ",'" + style + "'," + sid + ");");
+            if (result == 1) {
+                System.out.println("New Chef added successfully.");
+            } else {
+                System.out.println("Something went wrong. New chef was not added.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try { statement.close(); } catch (Exception e) { /* ignored */ }
+            try { con.close(); } catch (Exception e) { /* ignored */ }
         }
     }
 
+    /**
+     * Add a delivery guy into database
+     * @author Yingjie Xu
+     */
+    public static void addDeliveryGuy() {
+        Connection con = null;
+        Statement statement = null;
+        try {
+            con = DriverManager.getConnection(url, usernamestring, passwordstring);
+            statement = con.createStatement();
+            int sid = addStaff();
+            if (sid == -1) {
+                System.out.println("Fail to add the staff.");
+                return;
+            }
+            System.out.println("----- Info of Delivery_guy -----");
+            System.out.println("Phone number ('xxx-xxx-xxxx'): ");
+            String phone = sc.nextLine();
+            System.out.println("Delivery method (Car, Bike etc.): ");
+            String method = sc.nextLine();
+            int result = statement.executeUpdate("INSERT INTO delivery_guy VALUES('" + method + "','" + phone + "'," + sid + ");");
+            if (result == 1) {
+                System.out.println("New Delivery_guy added successfully.");
+            } else {
+                System.out.println("Something went wrong. New Delivery_guy was not added.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try { statement.close(); } catch (Exception e) { /* ignored */ }
+            try { con.close(); } catch (Exception e) { /* ignored */ }
+        }
+    }
+
+    /**
+     * Add a waiter into database
+     * @author Yingjie Xu
+     */
+    public static void addWaiter() {
+        Connection con = null;
+        Statement statement = null;
+        try {
+            con = DriverManager.getConnection(url, usernamestring, passwordstring);
+            statement = con.createStatement();
+            int sid = addStaff();
+            if (sid == -1) {
+                System.out.println("Fail to add the staff.");
+                return;
+            }
+            System.out.println("----- Info of Waiter -----");
+            System.out.println("Waiter doesn't require additional attributes.");
+            int result = statement.executeUpdate("INSERT INTO waiter VALUES(" + sid + ");");
+            if (result == 1) {
+                System.out.println("New waiter added successfully.");
+            } else {
+                System.out.println("Something went wrong. New waiter was not added.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try { statement.close(); } catch (Exception e) { /* ignored */ }
+            try { con.close(); } catch (Exception e) { /* ignored */ }
+        }
+    }
+
+    /**
+     * Modify the salary of a given sid
+     * @author Yingjie Xu
+     */
+    public static void modifySalary() {
+
+    }
+
+    /**
+     * All possible methods for the admin to modify the customer
+     * @author Hengxian Jiang
+     */
     public static void customer() {
         System.out.println("---------- Customer ----------");
         System.out.println("Please select from following options: ");
@@ -108,21 +273,67 @@ public class RestaurantAdmin {
         System.out.println("2 - Make a reservation");
         System.out.println("3 - Modify the address of a customer");
         System.out.println("4 - View the customer's info by phone number");
+        int select = sc.nextInt();
+        sc.nextLine();
+        if (select == 1) {
+
+        } else if (select == 2) {
+
+        } else if (select == 3) {
+
+        } else if (select == 4) {
+
+        } else {
+            System.out.println("Invalid selection.");
+            return;
+        }
     }
 
+    /**
+     * All possible methods for the admin to modify the menu
+     * @author Hengxian Jiang
+     */
     public static void menu() {
         System.out.println("---------- Menu ----------");
         System.out.println("Please select from following options: ");
         System.out.println("1 - Add a dish");
         System.out.println("2 - Modify the price of a dish");
         System.out.println("3 - View all the dishes");
+        int select = sc.nextInt();
+        sc.nextLine();
+        if (select == 1) {
+
+        } else if (select == 2) {
+
+        } else if (select == 3) {
+
+        } else {
+            System.out.println("Invalid selection.");
+            return;
+        }
     }
 
+    /**
+     * All possible methods for the admin to modify the order
+     * @author Helen Ren & Weiming Guo
+     */
     public static void order() {
         System.out.println("---------- Order ----------");
         System.out.println("Please select from following options: ");
         System.out.println("1 - Place a dine-in order");
         System.out.println("2 - Place a delivery order");
         System.out.println("3 - View order by oid");
+        int select = sc.nextInt();
+        sc.nextLine();
+        if (select == 1) {
+
+        } else if (select == 2) {
+
+        } else if (select == 3) {
+
+        } else {
+            System.out.println("Invalid selection.");
+            return;
+        }
     }
 }
